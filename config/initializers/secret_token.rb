921088b12ed869660b182651e1a0b932eb7ba325
FUKUI Osamu <ofukui@irubysystems.com>
2013-04-30 23:54:04 +0900

    etetest start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Etetest::Application.config.secret_key_base = 'e753d53b6fa1922b413f8ba80f3ffc9b77504762b9f6acea86b5612884e4a14520d6f5a4eb9b4078c6f62e44c631e284cae87eb2efdeb019d6b762b77513dac4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Etetest::Application.config.session_store :cookie_store, key: '_etetest_session'
